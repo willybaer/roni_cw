c06d0d309bb5dc3ca6ea03b9e7aa911eb2a6ace9
Wilhelm Dewald <wilhelmdewald@googlemail.com>
2017-02-05 12:10:54 +0100

    added additional german map squares

--- src/geo/config.py
+++ src/geo/config.py
@@ -31,9 +31,36 @@ bounding_boxes = {
     ],
     'germany': [
         # Schweix DE - Bregenz AT
+        # {
+        #     'top_left': [49.134604, 7.508044],
+        #     'bottom_right': [47.507108, 9.751487]
+        # },
+        # Mulfingen DE - Schönbergwald Österreich
         {
-            'top_left': [49.134604, 7.508044],
-            'bottom_right': [47.507108, 9.751487]
+            'top_left': [49.325079, 9.756004],
+            'bottom_right': [47.476335, 13.172752]
+        },
+        # Wolfheze NE - Lembach FR
+        {
+            'top_left': [51.998376, 5.800926],
+            'bottom_right': [49.005414, 7.800438]
+        },
+        # Elwangen DE
+        {
+            'top_left': [52.038948, 7.306053],
+            'bottom_right': [48.954947, 10.085594]
+        },
+        {
+            'top_left': [52.038955, 9.525291],
+            'bottom_right': [48.940524, 12.271873]
+        },
+        {
+            'top_left': [50.254215, 12.085105],
+            'bottom_right': [49.416322, 12.700339]
+        },
+        {
+            'top_left': [54.406129, 11.316062],
+            'bottom_right': [50.324411, 14.941550]
         }
     ]
 }
